8cb3a50b7c8101a47dd41d6bbf81acebb99e7d4f
Galaxy Tech. <mbonsu356@gmail.com>
2026-09-12 00:30:40 +0000

    Update config.js

--- NeutronBlaze/src/config.js
+++ NeutronBlaze/src/config.js
@@ -1,1 +1,17 @@
+require("dotenv").config();
 
+module.exports = {
+  botName: process.env.BOT_NAME || "NeutronBlaze",
+  prefix: process.env.PREFIX || ".",
+  ownerNumber: process.env.OWNER_NUMBER || "",
+  environment: process.env.NODE_ENV || "production",
+
+  cooldown: {
+    default: 3000
+  },
+
+  rateLimit: {
+    maxCommands: 20,
+    windowMs: 60000
+  }
+};
